ed63cf01cb63d17ce79c55636215ca0560eab0dd
Wenceslau Neto <wbaneto@gmail.com>
2024-05-24 22:09:03 +0100

    Banner gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
-    <div className="relative h-full lg:h-[140vh]">
+    <div className="relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]">
 
-      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+      <main className="relative pb-24 pl-4 lg:pl-16">
 
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
 
           <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black" >
 
             <Image
-              src="/banner_the_witcher.png"
-              alt="The Witcher"
+              src="/banner.png"
+              alt="MAID"
               fill={true}
               className="h-[65vh] object-cover object-top lg:h-[95vh]"
             />
@@ -21,13 +21,12 @@ export default function Home() {
           </div>
 
           <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl" >
-            The Wicher
+            Maid
           </h1>
 
           <p className="text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
-            The Witcher is a Polish-American fantasy drama streaming television series produced by Lauren Schmidt Hissrich.
-            It is based on the book series of the same name by Polish writer Andrzej Sapkowski. Set on a fictional, medieval-inspired landmass known as "the Continent",
-            The Witcher explores the legend of Geralt of Rivia and princess Ciri, who are linked to each other by destiny.
+            After a family tragedy turns her life upside down, 16-year-old high schooler Tooru Honda takes matters into her own hands and moves out...
+            into a tent! Unfortunately for her, she pitches her new home on private land belonging to the mysterious Souma clan, and it isn't long before the owners discover her secret. But, as Tooru quickly finds out when the family offers to take her in, the Soumas have a secret of their own--when hugged
           </p>
 
         </div>
