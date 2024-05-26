c545f6d541616d2e55de3eaa93542bab6be02690
Wenceslau Neto <wbaneto@gmail.com>
2024-05-26 17:55:02 +0100

    Login page 1

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div
+            style={{ backgroundImage: "url('/background.jpg')" }}
+            className="bg-cover bg-center bg-no-repeat h-screen w-screen text-white">
+
+            <div className="flex min-h-screen flex-col items-center justify-center py-2">
+                <h1>Auth Page</h1>
+            </div>
+
+        </div>
+    );
+}
