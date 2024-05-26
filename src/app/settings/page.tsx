24fa7e1612128457ca1b44dee919815e5936151d
Wenceslau Neto <wbaneto@gmail.com>
2024-05-26 16:09:58 +0100

    Component error

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getAppSettings } from "../lib/settings";
+import BuggyButton from "../components/BuggyButton";
 
 async function SettingsPage() {
 
@@ -11,6 +12,10 @@ async function SettingsPage() {
         <div className="border border-dashed border-red-500 p-4">
             <div>Theme: {theme}</div>
             <div>Language: {language}</div>
+        </div>
+        <div className="mt-4">
+
+            <BuggyButton />
         </div>
     </div>
 
--- src/app/settings/user/page.tsx
+++ src/app/settings/user/page.tsx
@@ -1,3 +1,4 @@
+import BuggyButton from "@/app/components/BuggyButton";
 import { getUserDate } from "@/app/lib/settings";
 import React from "react";
 
@@ -6,13 +7,20 @@ async function UserPage() {
     const { name, email } = await getUserDate();
 
 
-    return <div>
-        Settings
-        <div className="border border-dashed border-red-500 p-4">
-            <div>Name: {name}</div>
-            <div>Email: {email}</div>
+    return (
+        <div>
+            Settings
+            <div className="border border-dashed border-red-500 p-4">
+                <div>Name: {name}</div>
+                <div>Email: {email}</div>
+            </div>
+            <div className="mt-4">
+
+                <BuggyButton />
+            </div>
+
         </div>
-    </div>
+    )
 
 }
 
