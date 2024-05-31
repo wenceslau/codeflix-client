9f72ba0d4d00201309a873c6d8d8c28a7e92eeed
Wenceslau Neto <wbaneto@gmail.com>
2024-05-31 10:23:46 +0100

    Wrap Header component with React Suspense. To fix the error 'useSearchParams() should be wrapped in a suspense boundary at page "/".'
    Wrapped the Header component with React's Suspense for better loading performance. This aims to enhance user experience by providing a fallback UI while waiting for the component to render. The import line for React was also updated to include Suspense.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { UserProfile } from './UserProfile';
 import { Logo } from './Logo';
 import { NavLinks } from './NavLinks';
@@ -23,31 +23,33 @@ export default function Header() {
 
   const onSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-   const newParams = new URLSearchParams(params.toString());
+    const newParams = new URLSearchParams(params.toString());
     newParams.set('title', searchTerm);
     router.push(`/search?${newParams.toString()}`);
   };
 
   return (
-    <header
-      className={`${isScrolled && 'bg-black'} 
+    <Suspense>
+      <header
+        className={`${isScrolled && 'bg-black'} 
             fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}>
 
-      <div className="flex items-center space-x-2 md:space-x-8">
-        <Logo />
-        <NavLinks />
-      </div>
+        <div className="flex items-center space-x-2 md:space-x-8">
+          <Logo />
+          <NavLinks />
+        </div>
 
-      <div className="flex items-center space-x-2 md:space-x-8">
-        <SearchForm
-          searchTerm={searchTerm}
-          onSearchTermChange={onSearchTermChange}
-          onSearch={onSearch}
-        />
-        <UserProfile />
-      </div>
+        <div className="flex items-center space-x-2 md:space-x-8">
+          <SearchForm
+            searchTerm={searchTerm}
+            onSearchTermChange={onSearchTermChange}
+            onSearch={onSearch}
+          />
+          <UserProfile />
+        </div>
 
 
-    </header>
+      </header>
+    </Suspense>
   );
 }
